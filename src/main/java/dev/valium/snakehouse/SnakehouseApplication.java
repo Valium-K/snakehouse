f8719d95f3fe863ab240d07cd07df5b34558bd7c
Valium-K <lschan96@gmail.com>
2022-03-07 22:46:56 +0900

    init commit

---
+++ src/main/java/dev/valium/snakehouse/SnakehouseApplication.java
@@ -0,0 +1,13 @@
+package dev.valium.snakehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnakehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/valium/snakehouse/SnakehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.valium.snakehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
