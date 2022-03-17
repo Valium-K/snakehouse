38ed73a7854efcf7a99729c90b03aad7f33e9b90
Valium-K <lschan96@gmail.com>
2022-03-17 18:50:23 +0900

    기타 수정

--- src/main/java/dev/valium/snakehouse/module/exception/ExceptionController.java
+++ src/main/java/dev/valium/snakehouse/module/exception/ExceptionController.java
@@ -13,12 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/exception")
 public class ExceptionController {
 
-    @GetMapping(value = "/entrypoint")
+    @RequestMapping(value = "/entrypoint")
     public CommonResult entrypointException() {
         throw new AuthenticationEntryPointException("해당 리소스에 대한 접근 권한이 없습니다.");
     }
 
-    @GetMapping(value = "/access-denied")
+    @RequestMapping(value = "/access-denied")
     public CommonResult accessDeniedException() {
         throw new AccessDeniedException("보유한 권한으로 접근 할 수 없는 리소스 입니다.");
     }
