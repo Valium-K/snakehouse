2b20a8bd8759137e4fb4f45b5374ac9469eb2c64
Valium-K <lschan96@gmail.com>
2022-03-18 16:35:41 +0900

    레디스 컨트롤러 추가

--- src/main/java/dev/valium/snakehouse/module/basichello/HelloController.java
+++ src/main/java/dev/valium/snakehouse/module/basichello/HelloController.java
@@ -1,11 +1,17 @@
 package dev.valium.snakehouse.module.basichello;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Set;
+
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
+
+    private final RedisTemplate<String, ?> redisTemplate;
 
     @GetMapping("/hello-api")
     public String hello() {
@@ -16,5 +22,12 @@ public class HelloController {
     public String robots() {
         return "User-agent: *\n" +
                 "Disallow: /";
+    }
+
+    @GetMapping("/redis")
+    public String redis() {
+        Set<String> keys = redisTemplate.keys("*");
+
+        return keys != null ? keys.toString() : "[]";
     }
 }
